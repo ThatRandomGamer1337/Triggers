187b030a44618026257b0d7b8dfa62b91b06991a
ThatRandomGamer1337 <113189409+ThatRandomGamer1337@users.noreply.github.com>
2022-10-26 09:48:21 -0300

    Triggers Added + Messing around with

---
+++ Assets/Audio/Credits.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    // https://www.videvo.net/sound-effect/thunder-clap4-hvy-c-sdt2042602/259127/ is where i got the audio
+
+}
